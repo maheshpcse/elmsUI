706b908e132177dc70eb8c6afb70a22ea789ad42
maheshpcse <maheshmahi1599@gmail.com>
2022-01-24 23:10:59 +0530

    leaves history design updated

--- src/app/leaves/leaves-history/leaves-history.component.ts
+++ src/app/leaves/leaves-history/leaves-history.component.ts
@@ -7,6 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LeavesHistoryComponent implements OnInit {
 
+  empLeavesData: any = [
+    { leave_id: 1, leaveTypeId: 1, leaveTypeName: 'Casual Leave', leaveStartDate: '2022-01-02', leaveEndDate: '2022-01-04', leaveReason: 'Going to home', leaveAppliedDate: '2022-01-01', remarks: 'permission granted', status: 1},
+    { leave_id: 2, leaveTypeId: 1, leaveTypeName: 'Casual Leave', leaveStartDate: '2022-01-11', leaveEndDate: '2022-01-13', leaveReason: 'Sankranthi festival', leaveAppliedDate: '2022-01-10', remarks: 'permission granted', status: 1},
+    { leave_id: 3, leaveTypeId: 2, leaveTypeName: 'Sick Leave', leaveStartDate: '2021-12-10', leaveEndDate: '2022-01-11', leaveReason: 'Health checkup', leaveAppliedDate: '2021-12-09', remarks: 'permission not granted', status: 0},
+    { leave_id: 4, leaveTypeId: 3, leaveTypeName: 'Comp off', leaveStartDate: '2021-12-24', leaveEndDate: '2021-12-24', leaveReason: 'Christmas celebrations', leaveAppliedDate: '2021-12-23', remarks: 'permission not granted', status: 0},
+    { leave_id: 5, leaveTypeId: 4, leaveTypeName: 'Medical Leave', leaveStartDate: '2021-11-02', leaveEndDate: '2021-11-03', leaveReason: 'Eye care appointment', leaveAppliedDate: '2021-11-01', remarks: 'permission granted', status: 1},
+  ];
+
   constructor() { }
 
   ngOnInit() {
